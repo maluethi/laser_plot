9d0369886a56e02f5b4e3fe62b3fdb1397e58c7b
matthias <matthias.luethi@lhep.unibe.ch>
2017-07-02 18:52:23 +0200

    histogram selection is working now

--- track_ana.py
+++ track_ana.py
@@ -45,6 +45,7 @@ def get_histo(data, plane, bins=200):
 
 
 def update_plots(attr, old, new):
+    print(attr)
     if attr == '<':
         evt = int(event_select.value) - 1
         event_select.value = str(evt)
@@ -60,12 +61,30 @@ def update_plots(attr, old, new):
     # hit update
     wire = get_wire(data, evt, plane)
     tick = get_tick(data, evt, plane)
-    hit_source.data = dict(wire=wire, tick=tick)
+    peak = get_ampl(data, evt, plane)
+    hit_source_static.data = dict(wire=wire, tick=tick, peak=peak)
+    hit_source_selection.data = dict(wire=[], tick=[])
 
     # hist update
-    peak = get_ampl(data, evt, plane)
     hist, edges = get_histo(peak, plane=plane)
     hist_source.data = dict(hist=hist, edge_left=edges[:-1], edge_right=edges[1:])
+    hist_source_selection.data = dict(hist=hist, edge_left=edges[:-1], edge_right=edges[1:])
+
+
+def selection_change(attr, old, new):
+    max_idx = np.max(hist_source_selection.selected['1d']['indices'])
+    min_idx = np.min(hist_source_selection.selected['1d']['indices'])
+
+    bin_max = hist_source_selection.data['edge_left'][max_idx]
+    bin_min = hist_source_selection.data['edge_left'][min_idx]
+
+    print(bin_max, bin_min)
+
+    peak = hit_source_static.data['peak']
+
+    idx = np.where((peak < bin_max) & (peak > bin_min))
+    select_hits = dict(wire=hit_source_static.data['wire'][idx], tick=hit_source_static.data['tick'][idx])
+    hits_sel.data_source.data = select_hits
 
 
 base_dir = "/home/data/uboone/laser/7267/out/roi/"
@@ -74,8 +93,8 @@ data = LarData(base_dir + filename)
 data.read_ids()
 data.read_hits(planes="u")
 
-plt_hits = figure(title='hits', plot_width=1200, plot_height=300, x_range=[0, 3460], y_range=[0, 9600])
-plt_hist = figure(title="histo", plot_width=300, plot_height=300)
+plt_hits = figure(title='hits', plot_width=1200, plot_height=300, x_range=[0, 3460], y_range=[3200, 7000])
+plt_hist = figure(title="histo", plot_width=300, plot_height=300, tools='pan,wheel_zoom,xbox_select,reset')
 event_select = TextInput(value='1')
 back_btn = Button(label='<')
 fwrd_btn = Button(label='>')
@@ -89,17 +108,26 @@ tick = get_tick(data, event, plane)
 peak = get_ampl(data, event, plane)
 hist, edges = get_histo(peak, plane=plane)
 
-hit_source = ColumnDataSource(data=dict(wire=wire, tick=tick))
+hit_source_static = ColumnDataSource(data=dict(wire=wire, tick=tick, peak=peak))
+hit_source_selection = ColumnDataSource(data=dict(wire=[], tick=[]))
+
 hist_source = ColumnDataSource(data=dict(hist=hist, edge_left=edges[:-1], edge_right=edges[1:]))
+hist_source_selection = ColumnDataSource(data=dict(hist=hist, edge_left=edges[:-1], edge_right=edges[1:]))
 
 # histogram plot
 plt_hist.quad(top='hist', source=hist_source, bottom=0,
               left='edge_left', right='edge_right',
               fill_color="#036564", line_color="#033649",
               )
+hist_bkgnd = plt_hist.circle('edge_left', 'hist',
+                selection_color="orange",
+                source=hist_source_selection,
+                fill_color=None,
+                line_alpha=1.)
 
 # hit plot
-plt_hits.circle('wire', 'tick', source=hit_source)
+hits = plt_hits.circle('wire', 'tick', source=hit_source_static, fill_alpha=0.2)
+hits_sel = plt_hits.cross('wire', 'tick', source=hit_source_selection, color="orange",size=20)
 
 
 plots = row(plt_hits, plt_hist)
@@ -110,7 +138,8 @@ main_col = column(row(cntrl), plots)
 event_select.on_change('value', update_plots)
 back_btn.on_click(partial(update_plots, attr='<', old='', new=''))
 fwrd_btn.on_click(partial(update_plots, attr='>', old='', new=''))
-hist_source
+
+hist_source_selection.on_change('selected', selection_change)
 
 curdoc().add_root(main_col)
 curdoc().title = "Sliders"
