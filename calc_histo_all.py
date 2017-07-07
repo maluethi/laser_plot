a398978011f2670c28ad71fbafcaefcc41c2b99c
matthias <matthias.luethi@lhep.unibe.ch>
2017-07-07 11:57:46 +0200

    range adjustements implemented

--- calc_histo_all.py
+++ calc_histo_all.py
@@ -1,16 +1,18 @@
-
 from larana.lar_data import LarData
 import numpy as np
 from glob import glob1
+
 
 def get_plane_idx(dt, plane_id):
     if plane_id < 0 or plane_id > 2:
         raise ValueError("plane must be 0,1 or 2, it was {}".format(plane_id))
     return np.where(dt == plane_id)
 
+
 def get_ampl(dt, eventid, plane):
     hit = dt.get_hits(eventid)
     return hit.peak_amp[get_plane_idx(hit.plane, plane)]
+
 
 def get_integral(dt, eventid, plane):
     hit = dt.get_hits(eventid)
@@ -33,8 +35,8 @@ def get_width(dt, eventid, plane):
 
 # Get all the files
 outdir = "./data/"
-qualifier = "roi"
-base_dir = "/home/data/uboone/laser/7267/out/roi/"
+qualifier = "cosmic"
+base_dir = "/home/data/uboone/laser/7267/out/{}/".format(qualifier)
 filenames = sorted(glob1(base_dir, "LaserReco-*"))
 
 dims = ["amplitude", "width", "integral"]
@@ -45,21 +47,26 @@ edges = {dim: {plane: [] for plane in range(3)} for dim in dims}
 getters = {dim: getter for dim, getter in zip(dims, getters)}
 
 ampl_ranges = {0: [-1000, 0], 1: [0, 1000], 2: [0, 1000]}
-width_ranges = {0: [-1000, 0], 1: [0, 1000], 2: [0, 1000]}
-inte_ranges = {0: [-1000, 0], 1: [0, 1000], 2: [0, 1000]}
+ampl_bins = 500
+width_ranges = {0: [0,150], 1: [0, 150], 2: [0, 150]}
+width_bins = 150
+inte_ranges = {0: [0, 5000], 1: [0, 5000], 2: [0, 5000]}
+inte_bins = 1000
 
+ranges = {dim: r for r,dim in zip([ampl_ranges, width_ranges, inte_ranges],dims) }
+bins = {dim: bins for dim, bins in zip(dims, [ampl_bins, width_bins, inte_bins])}
 
 for filename in filenames:
+    print('processing file {}'.format(filename))
     data = LarData(base_dir + filename)
     data.read_hits(planes='u')
     data.read_ids()
 
-    bins = 200
-
     for idx in range(data.n_entries):
+
         for plane in range(3):
             for dim in dims:
-                hist, edge = np.histogram(getters[dim].__call__(data, idx, plane), bins=bins, range=ampl_ranges[plane])
+                hist, edge = np.histogram(getters[dim].__call__(data, idx, plane), bins=bins[dim], range=ranges[dim][plane])
                 histos[dim][plane].append(hist)
                 edges[dim][plane] = edge
 
@@ -70,9 +77,8 @@ for dim in dims:
         hist[dim][plane] = np.sum(histos[dim][plane], axis=0)
 
     dim_data = [[hist[dim][pl], edges[dim][pl]] for pl in range(3)]
-    print(dim_data)
-
-    np.save("./data/hist-{}-{}.npy".format(dim, qualifier), )
+    print("saving {}".format(dim))
+    np.save("./data/hist-{}-{}.npy".format(dim, qualifier), dim_data)
 
 
 
