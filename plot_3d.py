1ce1bf22cd6bf3536afe57bc3d6c7e47d714d656
matthias <matthias.luethi@lhep.unibe.ch>
2017-07-25 22:39:14 +0200

    first try on 3d plotting the distortion field.

---
+++ plot_3d.py
@@ -0,0 +1,29 @@
+
+import numpy as np
+import larana.lar_utils as laru
+
+from mayavi import mlab
+
+
+input_filename = '/home/matthias/workspace/my_fieldcorr/output/RecoDispl-exp-roii.root'
+input_filename = '/home/matthias/workspace/FieldCalibration/output/RecoDispl-100.root'
+
+
+dist_raw = laru.get_histos(input_filename)
+distortion = laru.make_array(dist_raw).view(np.recarray)
+
+x, y, z = np.meshgrid(np.linspace(laru.TPC.x_min, laru.TPC.x_max, distortion.shape[0]),
+                      np.linspace(laru.TPC.y_min, laru.TPC.y_max, distortion.shape[1]),
+                      np.linspace(laru.TPC.z_min, laru.TPC.z_max, distortion.shape[2]))
+
+distortion = laru.filter_max(distortion)
+src = mlab.pipeline.vector_field(distortion.dx, distortion.dy, distortion.dz)
+mlab.pipeline.vectors(src, mask_points=20, scale_factor=3.)
+flow = mlab.flow(distortion.dx, distortion.dy, distortion.dz, seed_scale=0.2,
+                          seed_resolution=10,
+                          integration_direction='both')
+mlab.outline()
+#mlab.pipeline.vector_cut_plane(src, mask_points=3, scale_factor=3)
+mlab.show()
+
+#mlab.pipeline.vectors(src, mask_points=20, scale_factor=3.)
