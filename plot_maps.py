f8a56d486f89948be734544dfa07e3b48a28dbcc
matthias <matthias.luethi@lhep.unibe.ch>
2017-09-01 11:04:26 +0200

    redesign plot to show all three dimensions in one plot

--- plot_maps.py
+++ plot_maps.py
@@ -8,7 +8,7 @@ from collections import namedtuple
 import larana.lar_utils as laru
 
 input_filename = '/home/matthias/workspace/my_fieldcorr/output/RecoDispl-exp-roii.root'
-input_filename = '/home/matthias/workspace/FieldCalibration/output/RecoCorrection-10.root'
+input_filename = '/home/matthias/workspace/FieldCalibration/output/RecoCorrection-Sim_same.root'
 
 
 dist_raw = laru.get_histos(input_filename)
@@ -19,30 +19,42 @@ x, y, z = np.meshgrid(np.linspace(laru.TPC.x_min, laru.TPC.x_max, distortion.sha
                       np.linspace(laru.TPC.z_min, laru.TPC.z_max, distortion.shape[2]))
 
 
-for sl in range(0, distortion.shape[2]):
+for sl in range(49, distortion.shape[2]):
 
-    f, ax, = plt.subplots(1,1, figsize=(8, 2.3), dpi=260)
+    f, ax, = plt.subplots(1, 3, figsize=(12, 5))
     #
-    dist = distortion[sl, : , :]
+    dist = distortion[:, : , sl]
 
-    #qu = ax.contourf(z[sl, :, :], x[sl, :, :], dist.dx, cmap=cm.Spectral, vmin=-20, vmax=20, interpolation=None)
-    #qu.cmap.set_over('#FFFFFF')
-    im = ax.imshow(dist.dx, cmap=cm.Spectral, vmin=-10, vmax=10, interpolation=None)
-    im.cmap.set_over('#FFFFFF')
-    #
-    #plt.xlabel("z [cm]")
-    #plt.ylabel("x [cm]")
-    #plt.xticks([0, 25, 50, 75, 100])
-    #plt.yticks([0, 12, 25])
+    dimens = {0: 'dx',
+              1: 'dy',
+              2: 'dz',}
 
-    #ax.set_xticklabels([0, 250, 500, 750, 1000])
-    #ax.set_yticklabels([0,128,256])
+    limits = {0: [-5, 0],
+              1: [-20, 20],
+              2: [-5, 5]}
 
-    #
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="2%", pad=0.05)
-    plt.colorbar(im, cax)
-    ax.invert_yaxis()
+    ax[1].set_title("z={:.1f} [cm]".format(sl * 1036/101))
+
+    for dim in range(3):
+        #qu = ax.contourf(z[sl, :, :], x[sl, :, :], dist.dx, cmap=cm.Spectral, vmin=-20, vmax=20, interpolation=None)
+        #qu.cmap.set_over('#FFFFFF')
+        im = ax[dim].imshow(dist[dimens[dim]].T, cmap=cm.Spectral, vmin=limits[dim][0], vmax=limits[dim][1], interpolation=None)
+        im.cmap.set_over('#FFFFFF')
+
+
+        ax[dim].set_xlabel("x [cm]")
+
+        ax[dim].set_xticks([0, 12, 25])
+        ax[dim].set_yticks([0, 12, 25])
+        ax[dim].set_yticklabels([-116, 0, 116])
+        ax[dim].set_xticklabels([0, 128, 256])
+
+        #
+        divider = make_axes_locatable(ax[dim])
+        cax = divider.append_axes("right", size="2%", pad=0.05)
+        cax.set_title("{} [cm]".format(dimens[dim]))
+        plt.colorbar(im, cax)
+        ax[dim].invert_yaxis()
 
     # ax.set_title("y=" + str((start_slice+idx )*((232)/25) - 116) +"cm")
     # #ax.set_title("y=" + str((start_slice + idx) * 10) + "cm")
@@ -50,4 +62,7 @@ for sl in range(0, distortion.shape[2]):
 
     #plt.savefig("output/dist-x_zx-{}.png".format(slice))
 
+    ax[0].set_ylabel("y [cm]")
+
+    plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
     plt.show()
