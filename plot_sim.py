0e8abfddec124396c6665fafe8346187e2824e87
matthias <matthias.luethi@lhep.unibe.ch>
2017-11-15 11:44:30 +0100

    moved plot_edges to larutils

--- plot_sim.py
+++ plot_sim.py
@@ -1,23 +1,15 @@
 from larana.laser_sim import *
 import matplotlib.pyplot as plt
-from larana.lar_utils import make_figure, plot_endpoints
+from larana.lar_utils import make_figure, plot_endpoints, plot_edges
 from larana.laser_sim import Laser, LASER_POS, convert_to_uboone
 import csv
 
-def plot_edges(axes, start, end, kwargs=None):
-    if kwargs is None:
-        axes[0].plot([start[2], end[2]], [start[0], end[0]], '-*')
-        axes[1].plot([start[2], end[2]], [start[1], end[1]], '-*')
-        axes[2].plot([start[0], end[0]], [start[1], end[1]], '-*')
-    else:
-        axes[0].plot([start[2], end[2]], [start[0], end[0]], **kwargs)
-        axes[1].plot([start[2], end[2]], [start[1], end[1]], **kwargs)
-        axes[2].plot([start[0], end[0]], [start[1], end[1]], **kwargs)
+basedir = "/home/matthias/workspace/larana/projects/out/lcs2/"
 
-basedir = "/home/matthias/workspace/larana/projects/out/"
+run_number = 6
 
 sim_file = "input.txt"
-lasr_file = "Run-1.txt"
+lasr_file = "Run-{}.txt".format(run_number)
 
 scale = 20
 
@@ -33,10 +25,10 @@ with open(basedir + sim_file) as f_sim, open(basedir + lasr_file) as f_las:
         polar = lsr.polar_tick2laser(float(line[3]))
         direc = convert_to_uboone(azimu, polar, 1.0, laser_id)
 
-        start = LASER_POS[laser_id]
+        start = [120., 0., 1075.] #LASER_POS[laser_id]
         end = [s + 1000 * m for s, m in zip(start, direc)]
 
-        plot_edges(axes, start, end, kwargs={"alpha": 0.1, "color": "green"})
+        plot_edges(axes, start, end, alpha=0.1, color="g")
 
     sim_reader = csv.reader(f_sim, delimiter=' ')
     for line in sim_reader:
@@ -45,6 +37,5 @@ with open(basedir + sim_file) as f_sim, open(basedir + lasr_file) as f_las:
             momen = [float(line[7 + idx]) for idx in range(3)]
             end = [s + scale * m for s, m in zip(start, momen)]
 
-            plot_edges(axes, start, end, kwargs={"linestyle": "--", "color": "black"})
-
+            plot_edges(axes, start, end, linestyle="--", color="black")
 plt.show()
