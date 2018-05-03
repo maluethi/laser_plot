b0605baba81b4e79268a6d031165fdf205f04aaf
matthias <matthias.luethi@lhep.unibe.ch>
2018-05-03 17:38:14 +0200

    ADD: Time / Potential units

--- plot_time_histo_th.py
+++ plot_time_histo_th.py
@@ -23,7 +23,13 @@ args = parser.parse_args()
 # main variables for this plot
 loc = [500,1750,3000]
 raw_event = 305
-
+time_limit = [460, 560]
+# ADC to units: fixed time scaling 1 ADC Tick = 0.5us
+#               12bit ADC 2Vpp
+tick2time = 0.5
+meas2volt = 0.488
+v_drift = laru.drift_speed(0.273) * 1E-6 # cm/us
+print(v_drift)
 # loading data
 res = np.load('/home/matthias/workspace/larana/projects/out/time-histo/histo-more.npy')
 raw = laru.read_raw('$LDATA/7275/rwa/RwaData-7275-006.root')
@@ -41,9 +47,13 @@ prop_cycle = plt.rcParams['axes.prop_cycle']
 colors = prop_cycle.by_key()['color'][:3]
 
 # now some processing
-maximas = np.concatenate(res, axis=1)
+maximas = np.concatenate(res, axis=1) * tick2time
 
-w = np.arange(0,3456)
+n_wires = maximas.shape[0]
+n_timeticks = maximas.shape[1]
+print(maximas.shape)
+
+w = np.arange(0,n_wires)
 b = np.ones(maximas.shape)
 x = np.transpose(w * b.T)
 
@@ -55,41 +65,40 @@ ax2 = fig.add_subplot(gs1[-1, :-1])
 ax3 = fig.add_subplot(gs1[-1, -1])
 
 # do the 2D histogram
-ax1.hist2d(x.flatten(), maximas.flatten(), bins=(3456, 2000))
-ax1.set_ylim([950, 1100])
+ax1.hist2d(x.flatten(), maximas.flatten(), bins=(np.arange(0,n_wires), np.arange(0,2000)/2))
+ax1.set_ylim(time_limit)
 #ax1.colorbar(label='N')
 ax1.set_xlabel('Wire')
-ax1.set_ylabel('ADC tick')
+ax1.set_ylabel('t[$\mu s$]')
 
 labels = list(range(len(loc)))
 print(labels)
-for x, y, s in zip(loc, len(loc)*[951], labels):
+for x, y, s in zip(loc, len(loc)*[time_limit[0]], labels):
     print(x)
     ax1.text(x+40,y,str(s+1), color='w')
 
-ax1.vlines(loc, 950, 1100, linewidth=1, linestyles='dashed', colors=colors)
+ax1.vlines(loc, time_limit[0], time_limit[1], linewidth=1, linestyles='dashed', colors=colors)
 
 # now we do the histograms and the fits
 lo = [1,2,2]
-xlimits = [950, 1150]
-x = np.arange(0,len(wire_data[:,0]))
-
+xlimits = np.array([950, 1150])
+x = np.arange(0,len(wire_data[:,0])) * tick2time
 wids = []
 
-
 for (i, l), c in zip(enumerate(loc), colors):
-    n, bins, patches = ax3.hist(maximas[l], bins=500, color=c, alpha=0.4)
+    n, bins, patches = ax3.hist(maximas[l], bins=np.ceil(n_timeticks * tick2time), color=c, alpha=0.4)
     raw_signal = -1* wire_data[:, i]
-    guess = np.argmax(raw_signal[xlimits[0]:xlimits[1]]) + xlimits[0]
+    guess = (np.argmax(raw_signal[xlimits[0]:xlimits[1]]) + xlimits[0]) * tick2time
 
     gmodel = Model(gaussian)
-    result = gmodel.fit(n, x=bins[:-1],amp=400, cen=998, wid=10)
+    result = gmodel.fit(n, x=bins[:-1], amp=400, cen=998/2, wid=10)
 
     ax3.plot(bins[:-1], result.best_fit, color=c, linestyle='--')
 
     # plot raw
-    res_raw = gmodel.fit(raw_signal, x=x, amp=400, cen=guess, wid=10)
-    ax2.plot(raw_signal, color=c, alpha=0.4)
+    print(guess)
+    res_raw = gmodel.fit(raw_signal, x=x, amp=400, cen=guess, wid=5)
+    ax2.plot(x, raw_signal, color=c, alpha=0.4)
     ax2.plot(x, res_raw.best_fit, color=c, linestyle='--')
 
 
@@ -98,13 +107,14 @@ for (i, l), c in zip(enumerate(loc), colors):
     cen = result.params['cen']
     wid = result.params['wid']
 
+
     textstr = 'amp: {:03.2f} +/- {:03.2f}\n' \
               'cen: {:03.2f} +/- {:03.2f}\n' \
               'wid: {:03.2f} +/- {:03.2f}'.format(amp.value, amp.stderr,
                                                   cen.value, cen.stderr,
                                                   wid.value, wid.stderr)
-    wids.append((cen.value, cen.stderr))
-
+    wids.append((cen.value, wid.value))
+    print(textstr)
     # these are matplotlib.patch.Patch properties
     #props = dict(boxstyle='round', facecolor=None, alpha=0.5)
     # place a text box in upper left in axes coords
@@ -114,20 +124,19 @@ for (i, l), c in zip(enumerate(loc), colors):
     #anchored_text = AnchoredText(textstr, loc=lo[i])
     #ax2.add_artist(anchored_text)
 
-    amp = result.params['amp']
 
 
 ax2.grid()
 ax2.set_axisbelow(True)
-ax2.set_xlim(xlimits)
-ax2.set_xlabel('ADC Tick')
-ax2.set_ylabel('Amplitude')
+ax2.set_xlim(time_limit)
+ax2.set_xlabel('t[$\mu s$]')
+ax2.set_ylabel('A [$mV$]')
 ax2.set_ylim([-10,35])
 
 ax3.grid()
 ax3.set_axisbelow(True)
-ax3.set_xlim(xlimits)
-ax3.set_xlabel('ADC Tick')
+ax3.set_xlim(time_limit)
+ax3.set_xlabel('t[$\mu s$]')
 ax3.set_ylabel('N')
 
 fig.tight_layout()
@@ -135,6 +144,6 @@ if not args.prod:
     plt.show()
 else:
     plt.savefig('./gfx/EField/time_histo.pdf')
-    with open('./gfx/EField/time_hist.txt', 'w') as f:
+    with open('./gfx/EField/time_histo.txt', 'w') as f:
         for idx, w in enumerate(wids):
             f.write("{},{:03.2f},{:03.2f}\n".format(idx, w[0], w[1]))
